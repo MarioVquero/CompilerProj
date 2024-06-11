3233cd83152e3e71c1a0cf345cc274fe2b6b9374
mariovaquero <mvaquero143@gmail.com>
2024-06-10 22:21:56 -0400

    half part 2

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Compiler Project
 
-Building a Compiler using a tutorial will try and change the language later 
+Building a Compiler using a tutorial will try and change the language later but currently runs a custom language called "Teeny Tiny"
+
+As of writing this starting part 2 of the tutorial
+
+Completed last time was Lexing, which breaks the input code into small pieces called tokens,
+
+presumably part 2 is Parsing, which verifies that the tokens are in an order that our language allows,
+
+and presumably part 3 is Emitting, which produces the appropriate C code.
 
 
 
---
+++ parse.py
@@ -0,0 +1,73 @@
+import sys
+from lex import *
+
+# Parser object keeps track of current token and checks if the code matches the grammar
+class Parser:
+    def __init__ (self, lexer):
+        self.lexer = lexer
+        self.curToken = None
+        self.peekToken = None
+        self.nextToken()
+        self.nextToken() # called twice to initialize current and peek
+
+    # return true if the current token matches:
+    def checkToken(self, kind):
+        return kind == self.curToken.kind
+
+    # return true if the next token matches:
+    def checkPeek(self,kind):
+        return kind == self.peekToken.kind
+
+    # Try to match current token. if not, error Advances the current token
+    def match(self,kind):
+        if not self.checkToken(kind):
+            self.abort("Expected " + kind.name + " , got " + self.curToken.kind.name)
+        self.nextToken()
+
+    # Advances the current token
+    def nextToken(self):
+        self.curToken = self.peekToken
+        self.peekToken = self.lexer.getToken()
+        # no need to worry about passing the EOF, lexer handles that.
+
+    def abort(self,message):
+        sys.exit("Error" + message)
+
+    # Production rules
+
+    # program ::= {statement}
+    def program(self):
+        print("PROGRAM")
+
+        # Parse all the statements in the program.
+        while not self.checkToken(TokenType.EOF):
+            self.statement()
+
+    def statement(self):
+        # Check the first token to see what kind of statement this is.
+        # "PRINT" (expressions | string)
+        if self.checkToken(TokenType.PRINT):
+            print("STATEMENT-PRINT")
+            self.nextToken()
+
+            if self.checkToken(TokenType.STRING):
+                # Simple string.
+                self.nextToken()
+
+            else:
+                # expect an expression.
+                self.expression()
+
+        # NEWLINE.
+        self.nl()
+    
+
+    # nl ::= '\n' +
+    def nl(self):
+        print("NEWLINE")
+
+        # Require at least one newline.
+        self.match(TokenType.NEWLINE)
+        # NOTE: We will allow extra newlines too, of course
+        while self.checkToken(TokenType.NEWLINE):
+            self.nextToken
